837c65e5ad095ec895cb2545e6a1e0e735fe7b14
Brian Teague <teague@mit.edu>
2018-03-02 09:39:11 -0500

    delete works

--- dptrp1/gui/gui_task.py
+++ dptrp1/gui/gui_task.py
@@ -7,16 +7,14 @@ Created on Feb 24, 2018
 import os, logging, requests
 
 # Enthought library imports.
-from envisage.api import Plugin, ExtensionPoint, contributes_to
+from envisage.api import Plugin, contributes_to
 from envisage.ui.tasks.api import TaskFactory
 from envisage.ui.tasks.action.preferences_action import PreferencesAction
 
-from pyface.tasks.api import Task, TaskLayout, PaneItem
-from pyface.tasks.action.api import DockPaneToggleGroup, SMenuBar, \
-    SMenu, SToolBar, TaskAction
-from pyface.api import error, ConfirmationDialog, FileDialog, \
-    ImageResource, YES, OK, NO, CANCEL, confirm
-from traits.api import on_trait_change, Instance
+from pyface.tasks.api import Task
+from pyface.tasks.action.api import SMenuBar, SMenu, SToolBar, TaskAction
+from pyface.api import error, FileDialog, OK, NO, confirm
+from traits.api import Instance
 from traitsui.api import Controller
 
 from dptrp1.gui.gui_panes import DPTPane
@@ -55,34 +53,11 @@ class GUITask(Task, Controller):
 #                                       image=ImageResource('document_open')),
                            PreferencesAction(name = "Register..."))]
     
-    
-
-    ###########################################################################
-    # 'Task' interface.
-    ###########################################################################
-
-#     def _default_layout_default(self):
-#         return TaskLayout(
-#             left=PaneItem('example.python_script_browser_pane'))
-
-#     def activated(self):
-#         """ Overriden to set the window's title.
-#         """
-#         filename = self.window.central_pane.editor.path
-#         self.window.title = filename if filename else 'Untitled'
 
     def create_central_pane(self):
         """ Create the central pane: the script editor.
         """
         return DPTPane(model = self.model)
-
-#     def create_dock_panes(self):
-#         """ Create the file browser and connect to its double click event.
-#         """
-#         browser = PythonScriptBrowserPane()
-#         handler = lambda: self._open_file(browser.selected_file)
-#         browser.on_trait_change(handler, 'activated')
-#         return [ browser ]
 
     ###########################################################################
     # 'ExampleTask' interface.
@@ -167,13 +142,14 @@ class GUITask(Task, Controller):
         parent = selected[0].parent_folder
 
         for s in selected:
-            # remove from device
+            if isinstance(selected, Folder):
+                self.dp.delete(s.entry_id)
+            else: #file
+                self.dp.delete_folder(s.entry_id)
             
             # remove from model -- which removes it from the tree
-            # TODO - handle folder
             parent.files.remove(s)
         
-        pass
 
     def _upload_files(self, editor):
         
@@ -223,67 +199,6 @@ class GUITask(Task, Controller):
         pass
                 
             
-        
-#         """ Shows a dialog to open a file.
-#         """
-#         dialog = FileDialog(parent=self.window.control, wildcard='*.py')
-#         if dialog.open() == OK:
-#             self._open_file(dialog.path)
-
-    def register(self):
-        pass
-#         """ Attempts to save the current file, prompting for a path if
-#             necessary. Returns whether the file was saved.
-#         """
-#         editor = self.window.central_pane.editor
-#         try:
-#             editor.save()
-#         except IOError:
-#             # If you are trying to save to a file that doesn't exist, open up a
-#             # FileDialog with a 'save as' action.
-#             dialog = FileDialog(parent=self.window.control,
-#                                 action='save as', wildcard='*.py')
-#             if dialog.open() == OK:
-#                 editor.save(dialog.path)
-#             else:
-#                 return False
-#         return True
-
-    ###########################################################################
-    # Protected interface.
-    ###########################################################################
-
-#     def _open_file(self, filename):
-#         """ Opens the file at the specified path in the editor.
-#         """
-#         if self._prompt_for_save():
-#             self.window.central_pane.editor.path = filename
-#             self.activated()
-# 
-#     def _prompt_for_save(self):
-#         """ Prompts the user to save if necessary. Returns whether the dialog
-#             was cancelled.
-#         """
-#         if self.window.central_pane.editor.dirty:
-#             message = 'The current file has unsaved changes. ' \
-#                       'Do you want to save your changes?'
-#             dialog = ConfirmationDialog(parent=self.window.control,
-#                                         message=message, cancel=True,
-#                                         default=CANCEL, title='Save Changes?')
-#             result = dialog.open()
-#             if result == CANCEL:
-#                 return False
-#             elif result == YES:
-#                 if not self.save():
-#                     return self._prompt_for_save()
-#         return True
-# 
-#     @on_trait_change('window:closing')
-#     def _prompt_on_close(self, event):
-#         """ Prompt the user to save when exiting.
-#         """
-#         if not self._prompt_for_save():
-#             event.veto = True
 
 class GUITaskPlugin(Plugin):
     """
